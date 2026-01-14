ac1fa993b493b62ed90b8cbc3912451c77feb3bd
kalrach <jasonea95@gmail.com>
2026-01-13 22:30:16 -0500

    Added zsh aliases

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# Packages
+alias ps="sudo pacman -S"
+alias psu="sudo pacman -Syu"
+alias psi="sudo pacman -Si"
+alias pqi="sudo pacman -Qi"
+alias ysu="yay -Syu"
+alias ys="yay -S"
+alias ysi="yay -Si"
+alias fpu="flatpak update"
+
+# QoL
+alias v="nvim"
+alias t="tmux"
+alias ff="fastfetch"
+
+# dotfiles
+alias config='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
